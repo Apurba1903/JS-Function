8e872562b9162431a8a31456903d02b43db48a9a
Apurba1903 <apurba1903@gmail.com>
2023-08-09 01:02:46 +0600

    object

---
+++ object.js
@@ -0,0 +1,23 @@
+var student = {
+    id: 360,
+    name: 'Apurba',
+    intake: 44,
+    section: 2
+}
+var mobile = {
+    brand: 'Iphone',
+    model: '11',
+    price: 38000,
+    camera: '20MP'
+}
+
+var myComputer = {
+    brand: "asus",
+    price: 39000,
+    color: 'Black',
+    processor: 'i7'
+}
+
+console.log(myComputer)
+myComputer.price = 80000;
+console.log(myComputer)
