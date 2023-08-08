57b125745c7811fc11da39bd96ff93a58ccf424f
Apurba1903 <apurba1903@gmail.com>
2023-08-09 00:47:29 +0600

    add

---
+++ add.js
@@ -0,0 +1,10 @@
+function add(num1 , num2){
+    var sum = num1 + num2;
+    return sum;
+}
+
+var result1 = add(10, 15);
+var result2 = add(13, 12);
+
+var finalResult = add(result1, result2);
+console.log(finalResult);
