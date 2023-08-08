42d39ec6710c900a6df5a24b50c1efa95d5dfaf3
Apurba1903 <apurba1903@gmail.com>
2023-08-08 23:19:43 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,8 @@
+// Function Declaration
+function startFan(){
+    console.log('Wake up');
+    console.log('Put a little brush and');
+    console.log('make up');
+}
+// Function Exacute
+startFan();
---
+++ parameter.js
@@ -0,0 +1,24 @@
+function bringSingara(money){
+    console.log('Take this money' , money);
+    console.log('Give me Singara');
+    
+}
+var taka = 500;
+bringSingara(taka);
+
+
+function add(num1 , num2){
+    console.log('Add this number', num1, num2);
+}
+
+add(125 , 250);
+
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(15 , 20, 25, 36, 20);
+
