585077e33c458fdd7935eb2ece4fbe55e51c3948
Apurba1903 <apurba1903@gmail.com>
2023-08-09 02:06:48 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,43 @@
+// If Else Condition check all the conditions and then gives output
+const color = 'yellow';
+if (color === 'green'){
+    console.log('Your are a green friend');
+}
+else if (color === 'blue'){
+    console.log('Your are a blue friend');
+}
+else if (color === 'red'){
+    console.log('Your are a red friend');
+}
+else if (color === 'white'){
+    console.log('Your are a white friend');
+}
+else if (color === 'yellow'){
+    console.log('Your are a yellow friend');
+}
+else{
+    console.log('Your are a black friend')
+}
+
+// Switch
+switch (color){
+    case 'green':
+        console.log('Your are a green friend');
+        break;
+    case 'blue':
+        console.log('Your are a blue friend');
+        break;
+    case 'white':
+        console.log('Your are a white friend');
+        break;
+    case 'red':
+        console.log('Your are a red friend');
+        break;
+    case 'yellow':
+        console.log('Your are a yellow friend');
+        break;
+
+    default:
+        console.log('Your are a black friend');
+}
+
